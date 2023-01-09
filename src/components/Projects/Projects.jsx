a132beb2382e5c4a072a53d311592f17546f5f57
Tomas Paoletti Velado <tomas.paolettiv@hotmail.com>
2023-01-09 12:36:01 -0300

    Se añade proyecto

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,6 +5,7 @@ import riganoHome from './assets/riganoHome.png';
 import tuPrestamo from './assets/tuPrestamo.png';
 import bukaTemplo from './assets/bukaTemplo.png';
 import travelPack from './assets/travelPack.png';
+import rickAndMortyAPI from './assets/rickAndMortyAPI.jpg';
 import "./Projects.css"
 
 
@@ -12,6 +13,14 @@ function Projects() {
 
   return (
     <Container className="container-projects">
+      <Card className="text-center">
+        <Card.Img src={rickAndMortyAPI} />
+        <CardProjects title="Rick and Morty API"
+          text="Rick and Morty cuenta bancaria es una web que simula manejar las cuentas de cada uno de los personajes de esta API convirtiéndolos en usuarios. Cada usuario puede depositarse dinero en su cuenta, realizar transferencias entre ellos, pedir un préstamo y pagar esa cuota del préstamo pedido. Todo se ve en tiempo real. Las tecnologías que utilice son React, Bootstrap-react, react-icons, sweetAlert y Uiball. El login es mediante Firebase"
+          git="https://github.com/TomasPaoletti/RickAndMortyAPI"
+          pag=" "
+        />
+      </Card>
       <Card className="text-center">
         <Card.Img src={travelPack} />
         <CardProjects title="Travelpack"
